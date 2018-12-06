27a8c3b3231b022d935e95450c6c4e99575d7946
CryptideAccGitHub <45621274+CryptideAccGitHub@users.noreply.github.com>
2018-12-06 16:48:04 +0700

    Update init.lua

--- base/init.lua
+++ base/init.lua
@@ -1,1 +1,13 @@
+ENT.Table = {
+name = 
+model = "models/error.mdl",
+}
 
+--Custom setting up
+function ENT:CustomOnInitialize() return end
+
+function ENT:Initialize()
+self:SetModel(self.Table["model"])
+self.Table["model"] = "var"
+self:CustomOnInitialize()
+end
