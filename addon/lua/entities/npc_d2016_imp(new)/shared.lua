2dae53332f19d159af8111e695ab5d3d8e6c7a53
CryptideAccGitHub <45621274+CryptideAccGitHub@users.noreply.github.com>
2019-03-15 16:48:33 +0700

    Update shared.lua

--- addon/lua/entities/npc_d2016_imp(new)/shared.lua
+++ addon/lua/entities/npc_d2016_imp(new)/shared.lua
@@ -1,1 +1,12 @@
+ENT.Base = "npc_cpt_base"
+ENT.Type = "ai"
+ENT.PrintName = "Imp"
+ENT.Author = "REXMaster"
+ENT.Contact = "Me" 
+ENT.Purpose = "Imp SNPC from DOOM (2016)"
+ENT.Instructions = "Rip and tear"
+ENT.Information	= "Small enemy, easily killable and not dangerous."  
+ENT.Category = "DOOM (2016)"
 
+ENT.Spawnable = false
+ENT.AdminSpawnable = false
