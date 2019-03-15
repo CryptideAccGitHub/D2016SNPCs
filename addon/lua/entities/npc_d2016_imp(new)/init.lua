87ae3c889f98ab62bcd43a0c858380aea2f9a245
CryptideAccGitHub <45621274+CryptideAccGitHub@users.noreply.github.com>
2019-03-15 19:01:13 +0700

    Update init.lua

--- addon/lua/entities/npc_d2016_imp(new)/init.lua
+++ addon/lua/entities/npc_d2016_imp(new)/init.lua
@@ -28,11 +28,115 @@ ENT.BloodEffect = {"blood_impact_red_01"}
 ENT.tbl_Animations = {}
 ENT.tbl_Capabilities = {CAP_OPEN_DOORS}
 
+--Custom set-up
+ENT.ISD2016NPC = true
+ENT.NEXTCTASK = CurTime()+1
+ENT.NEXTHEAVYATTACK = CurTime()+math.random(3,8)
+ENT.NEXTCSTATE = ""
+ENT.NEXTATTACK = CurTime() + 1
+ENT.CSTATE = ""
+ENT.CTARGET = self:GetPos()
+ENT.NEXTCTARGET = CurTime()+1
+
 function ENT:SetInit()
 	self:SetHullType(HULL_MEDIUM)
-	self:SetCState("Idle")
 	self:SetMovementType(MOVETYPE_STEP)
 	self:SetIdleAnimation("idle")
+	
+	self.CSTATE("Idle_NoEnemy")
+	self.CanWander = false
+	DEMON_COUNT = DEMON_COUNT+1
 end
+
+--Schedule processing--------------------------------------------------------------------------------------------
+
+function ENT:HandleSchedules(enemy,dist,nearest,disp,time)
+	if self:CanPerformProcess() then
+		
+		if self.CSTATE ~= "Idle_NoEnemy" and self.EnemyMemoryCount < 1 then
+		self.CSTATE = "InFight_NoEnemy"
+		end
+		
+		if dist < 150 and self:GetCurrentAnimation() == "idle" then
+			self:PlayActivity("meleeforward0"..math.random(1,2))
+		elseif dist < 160 and self:GetCurrentAnimation() == "run" then
+			self:PlayActivityENTERANMOVEMENTMELEEACTIVITYFAGGOT --("meleeforward0"..math.random(1,2))
+		end
+		
+		-- Just an id;e
+		
+		if self.CSTATE == "Idle_NoEnemy" then
+			if self:GetEnemy() != nil then
+				self:PlayActivity({\PLACE-ACTIVITY-TABLE-HERE-MOTHERFUCKER\})
+				self.CanWander = true
+				self.CSTATE = "InFight_Running"
+			end
+			
+		-- It runs to enemy/around enemy.
+			
+		elseif self.CSTATE == "InFight_Running" then
+			if self.NEXTCSTATE <= CurTime() and dist > 600 and dist < 1000  then
+				self.CSTATE = "InFight"
+				self.NEXTCSTATE = CurTime()+math.Rand(10,15)
+			elseif dist < 200 then
+				if self:Health <= 110 then self:RunAway() else self:ChaseEnemy() end
+				self.NEXTCSTATE = CurTime()+math.Rand(3,5)
+			end
+			
+			if self:FindInCone(enemy,90) and dist > 300 then
+			end
+			
+			if dist > 700 then
+				self:ChaseEnemy()
+			else
+				if self.NEXTCTARGET <= self:CurTime() then
+					/INSERT-MOVE-CODE-HERE-FAGGOT/
+				end
+			end
+		
+		-- It stands and throws its shit.	
+		elseif self.CSTATE == "InFight"
+			if (self.NEXTCSTATE <= CurTime()) or (dist > 1000)  then
+				self.CSTATE = "InFight_Running"
+				self.NEXTCSTATE = CurTime()+math.Rand(10,15)
+			elseif dist < 300 then
+				if self:Health <= 120 then self:RunAway() else self:ChaseEnemy() end
+				self.NEXTCSTATE = CurTime()+math.Rand(3,5)
+			end
+		end
+	end
+end
+	
+function ENT:OnThink()
+end
+
+--Utility code--------------------------------------------------------------------------------------------
+
+function ENT:OnRemove()
+	DEMON_COUNT = DEMON_COUNT-1
+end
+
+
+function ENT:OnDeath(dmg,dmginfo,hitbox)
+	DEMON_COUNT = DEMON_COUNT-1
+	if dmg:GetDamage() => 100 then
+		self.HasDeathRagdoll = false
+		for i=1,math.random(4,6) do
+			local gib = ents.Create("obj_d2016_gib")
+			gib:SetPos(self:GetPos() +self:OBBCenter())
+			gib:SetAngles(Angle(math.random(0,360),math.random(0,360),math.random(0,360)))
+			gib:SetOwner(self)
+			gib:Spawn()
+			gib:Activate()
+			local phys = spit:GetPhysicsObject()
+			if IsValid(phys) then
+				phys:SetVelocity(Vector(math.Rand(-100,100),math.Rand(-100,100),math.Rand(-100,100)) *2 +self:GetUp() * 200 + dmg:GetDamageForce())
+			end
+		end
+	end
+end
+
+
+
 
 
